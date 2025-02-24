6a4d32731d3b332736ef14e9941c8f0a3d0ff779
John Hu <johnhu1235@gmail.com>
2025-02-24 01:10:55 -0500

    added constants and some conditional loops

---
+++ README.md
@@ -0,0 +1,6 @@
+## How to Use
+```python
+python nussinov.py 2 GGCUAG off
+```
+## Citations
+[https://math.mit.edu/classes/18.417/Slides/rna-prediction-zuker.pdf] 
--- nussinov.py
+++ nussinov.py
@@ -1,16 +1,41 @@
+import sys
+from graphviz import Digraph
 #initialize dp matrix
-#dp[i][j] = maximal number of bp for subseq from i->j
+#dp[i][j] = maximal number of bp for subseq from i->j, seek to minimize G
+#very similar to landau levenshtein mods
 
+# Background: 
 # RNA is made up of {A, U, C, G}
 #watson crick {(A, U), (C, G)}
-# (G, U) wobble pair
+# (G, U) wobble pair for codon-anticodon
 
+MIN_LOOP_LENGTH, SEQUENCE = sys.argv[0], sys.argv[1]
 #minimal loop length = minimum unpaired bases in a loop
 
-#dp[i, j]= 
-# 0 if i>j
-# max(dp[i+1, j], dp[i, j-1]) and at least one unpaired
-# max(dp[i+1, j], dp[i, j-1], dp[i+1, j+1]+1), pair i<->j
-# for all j, i<k<j
-#   state = 1 if i and k pair else 0
-#   max(dp[i+1, k-1]+dp[k+1, j]) if i pairs with k
+N = len(SEQUENCE)
+dp = [[] * N]
+[dp[i][j] == 0 if i==j else 0 for i in range (0, N) for j in range(0, N)]
+
+
+valid_pairs = set([('A','U'), ('U','A'), ('C', 'G'), ('G','C'), ('G', 'U'), ('U', 'G')])
+def isPair(x, y):
+    return (x, y) in valid_pairs
+
+i = 1
+j = 10
+if i>j:
+    dp[i][j] = 0
+    
+else:
+    #if i or j is unpaired
+    IJUnpaired = dp[i+1, j], dp[i, j-1]
+    # if pairing valid for i<->j
+    IJPaired = max(dp[i+1, j], dp[i, j-1], dp[i+1, j-1]+1)
+    state = lambda i, k: 1 if isPair(i, k) else 0
+    #iff pairing  possible for i<->k
+    for k in range(i+1, j):
+        Bifurcate = max(dp[i+1, j], dp[i, j-1], dp[i+1, k-1]+dp[k+1, j]+state(i, k))
+    dp[i, j] = max(IJUnpaired, IJPaired, Bifurcate) 
+
+# TRACEBACK
+# suboptimals track -delBP 
