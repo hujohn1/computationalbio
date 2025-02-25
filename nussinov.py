cd3e4cc2204bf36914cb50c1733e968e25ad7ce0
John Hu <johnhu1235@gmail.com>
2025-02-25 10:37:00 -0500

    working versino of nussinov matrix filling with 1-based index instead of 0 now

--- nussinov.py
+++ nussinov.py
@@ -1,4 +1,5 @@
 import sys
+import types
 import numpy as np
 from graphviz import Digraph
 #initialize dp matrix
@@ -9,7 +10,7 @@ from graphviz import Digraph
 #watson crick {(A, U), (C, G)}
 # (G, U) wobble pair for codon-anticodon
 
-MIN_LOOP_LENGTH, SEQUENCE = int(sys.argv[1]), sys.argv[2]
+#MIN_LOOP_LENGTH, SEQUENCE = int(sys.argv[1]), sys.argv[2]
 #minimal loop length = minimum unpaired bases in a loop
 
 valid_pairs = set([('A','U'), ('U','A'), ('C', 'G'), ('G','C'), ('G', 'U'), ('U', 'G')])
@@ -18,36 +19,43 @@ def isPair(x, y):
 #tests
 print(isPair('A', 'C'))
 
+
+def getDP(dp, i, j, SEQ):
+    if(i>j):
+        dp[i][j] = 0   
+        return 
+    else:          
+        #if i or j is unpaired
+        IJUnpaired = max(dp[i+1, j], dp[i, j-1])
+        # if pairing valid for i<->j
+        #revert to 0-indexing for the string
+        state = lambda x, y: 1 if isPair(SEQ[x-1], SEQ[y-1]) else 0
+        IJPaired = dp[i+1, j-1] + state(i, j)
+        #iff pairing  possible for i<->k
+        if i < j - 1:
+            Bifurcate = max([dp[i, k] + dp[k+1, j] for k in range(i, j-1)])
+        else: Bifurcate = 0
+        dp[i, j] = max(IJUnpaired, IJPaired, Bifurcate) 
+
 def forwardsweep(SEQUENCE):
     N = len(SEQUENCE)
-    dp = np.array([[0 for j in range(N)] for i in range(N)])
+    dp = np.array([[-1 for j in range(N+1)] for i in range(N+1)])
     #diagonal sweep from major diagonal uprightwards
-    for diag in range(2, N+1):
-        for i in range(N-diag+1):
-            j = i+diag-1
-            if(i>j):
-                dp[i][j] = 0    
-            else:
-                #if i or j is unpaired
-                IJUnpaired = max(dp[i+1, j], dp[i, j-1])
-                # if pairing valid for i<->j
-                IJPaired = max(dp[i+1, j], dp[i, j-1], dp[i+1, j-1]+1) if isPair(SEQUENCE[i], SEQUENCE[j]) else 0
-                state = lambda i, k: 1 if isPair(i, k) else 0
-                #iff pairing  possible for i<->k
-                for k in range(i+1, j):
-                    Bifurcate = max(dp[i+1, j], dp[i, j-1], dp[i+1, k-1]+dp[k+1, j]+state(i, k))
-                    dp[i, j] = max(IJUnpaired, IJPaired, Bifurcate) 
+    for i in range(1, N+1):
+        dp[i, i] = 0
+    for i in range(2, N+1):
+        dp[i, i-1] = 0
 
+    for diag in range(2, N+1):
+        for j in range(diag, N+1):
+            i = j-diag+1
+            getDP(dp, i, j, SEQUENCE)
     return dp
-print(forwardsweep("GGUCCAC"));
+print(forwardsweep("GGGAAAUCC"));
 
 # TRACEBACK
-# suboptimals track -delBP 
-
-# Dot notation converter
-
-#connections = []
-#def recurse(i, j):
+#def traceback(dp: ):
+#    def recurse(i, j):
 #    if(i>j):
 #        return
 #    if(dp[i+1, j]==dp[i][j]):
@@ -56,3 +64,8 @@ print(forwardsweep("GGUCCAC"));
 #    elif(dp[i, j-1]==dp[i][k]):
 #        connections.append(i, j)
 #        recurse(i+1, j)
+# suboptimals track -delBP 
+
+# Dot notation converter
+
+connections = []
